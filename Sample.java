cb8ee94a49ad8ae643fef4b0037fc3d690ef18e9
PreetiBhatia <preeti.bhatia@apisero.com>
2021-08-16 11:56:22 +0530

    SOme msg

---
+++ Sample.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Sample{
+public static void main(String args[])
+{
+System.out.println("This is my first git file....");
+
+System.out.println("This is my second git file....");
+
+System.out.println("This is my third git file....and will be my second tag");
+
+
+
+System.out.println("This is my second gt file....");
+System.out.println("Editing first time on Big Bucket.......");
+
+
+System.out.println("Editing first time on Cpoy1.......");
+
+System.out.println("Editing first time on AP1.......");
+System.out.println("Apple.......");
+System.out.println("Strawberry.......");
+System.out.println("Mango.......");
+System.out.println("Litchi.......");
+System.out.println("BlueBerry.......");
+
+
+System.out.println("Making changes for jenkins.................");
+
+System.out.println("Making changes for jenkins for cloneee.................");
+}
+}
